df38cc5f60519f18bf0eb2ab27f884c4b7719822
Teletrup <saletrapotasowatosyf@gmail.com>
2023-05-22 17:37:30 +0200

    grumbly wumbly notes

---
+++ preact/test2/app.js
@@ -0,0 +1,33 @@
+import 'https://cdn.skypack.dev/preact/debug';
+import {h, render} from 'https://cdn.skypack.dev/preact';
+import {useReducer} from 'https://cdn.skypack.dev/preact/hooks'; //development version?
+import htm from 'https://cdn.skypack.dev/htm';
+
+const ht = htm.bind(h);
+
+
+const init = () => {
+  return 0;
+}
+
+
+const update = (state, msg) => {
+  const [action , data] = msg;
+  switch (action) {
+    case 'inc':
+      return state + 1;
+  }
+}
+
+
+const App = props => {
+  const [state, send] = useReducer(update, init());
+  return ht`
+    <h1>Hello ${state} </>
+    <br />
+    <button onclick=${() => send(['inc', 1])}>+</>
+  `;
+}
+
+
+render(ht`<${App} />`, document.body);
